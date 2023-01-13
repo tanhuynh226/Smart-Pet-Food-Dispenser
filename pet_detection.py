072c26b540d20c0bb5462f05031650b9c2fd3cee
ssaw098 <78987339+ssaw098@users.noreply.github.com>
2023-01-13 15:25:45 -0800

    Update pet_detection.py

--- pet_detection.py
+++ pet_detection.py
@@ -65,4 +65,53 @@ def detect_dog_breed(photo):
         return labels[0]['Name']
     else:
         return None
+    
+#ultrasonic ranger code
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO_TRIGGER = 18
+GPIO_ECHO = 24
+
+GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
+GPIO.setup(GPIO_ECHO, GPIO.IN)
+
+#calculates the distance that the ranger is receiving
+def distance():
+    GPIO.output(GPIO_TRIGGER, True)
+    
+    
+    time.sleep(0.00001)
+    GPIO.output(GPIO_TRIGGER, False)
+    
+    StartTime = time.time()
+    StopTime = time.time()
+    
+    while GPIO.input(GPIO_ECHO) ==0:
+        StartTime = time.time()
+        
+    while GPIO.input(GPIO_ECHO) ==1:
+        StopTime = time.time()
+        
+    TimeElapsed = StopTime-StartTime
+    
+    distance= (TimeElapsed*34300)/2
+    
+    return distance
+
+if __name__ == '__main__':
+    try:
+        #this only displays the distance 
+        while True:
+            dist = distance()
+            print("Measured distance = %.1f cm" % dist)
+            time.sleep(.5)
+            #testing "motion detection"
+            #if (dist2-dist1) >  
+            
+    except KeyboardInterrupt:
+        print("Stopped_________")
+        GPIO.cleanup()
 
