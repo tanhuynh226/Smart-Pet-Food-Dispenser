07cfb7a4551d4f942a545930cdddd7938e6fd148
Stella Zhou <78987339+ssaw098@users.noreply.github.com>
2023-02-15 13:38:55 -0800

    Average ultrasonic distance

--- ultrasonic_class.py
+++ ultrasonic_class.py
@@ -11,23 +11,28 @@ class UltrasonicRanger:
         GPIO.setup(echo, GPIO.IN)
 
     def distance(self, trigger, echo):
+        dist_arr = []
         GPIO.output(trigger, True)
     
         time.sleep(0.00001)
         GPIO.output(trigger, False)
         
-        startTime = time.time()
-        stopTime = time.time()
-        
-        while GPIO.input(echo) == 0:
+        while(len(dist_arr) < 5):
             startTime = time.time()
-            
-        while GPIO.input(echo) == 1:
             stopTime = time.time()
-            
-        timeElapsed = stopTime - startTime
-        
-        distance = (timeElapsed * 34300) / 2
+
+            while GPIO.input(echo) == 0:
+                startTime = time.time()
+
+            while GPIO.input(echo) == 1:
+                stopTime = time.time()
+
+            timeElapsed = stopTime - startTime
+            dist_arr.append((timeElapsed * 34300) / 2)
+        sum = 0
+        for x in dist_arr:
+            sum += x
+        distance = sum/len(dist_arr)
         
         return distance
 
