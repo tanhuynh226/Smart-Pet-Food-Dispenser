e795854ad3da8a8274e59eedb0bd77d5d548e502
Tan Huynh <47208261+tanhuynh226@users.noreply.github.com>
2023-02-19 04:16:21 -0800

    No need for ENABLE and DIR pins in dispense function

--- stepper_class.py
+++ stepper_class.py
@@ -13,7 +13,7 @@ class StepperMotor:
         GPIO.output(dir_pin, 1) # clockwise
         GPIO.output(en_pin,GPIO.LOW)
 
-    def dispense(self, en_pin, dir_pin, step_pin, seconds):
+    def dispense(self, step_pin, seconds):
         start = time.time()
 
         while time.time() < start + seconds:
@@ -29,8 +29,8 @@ class StepperMotor:
 if __name__ == '__main__':
     stepper_one = StepperMotor()
     stepper_one.setup(constant.STEPPER_ONE_ENABLE, constant.STEPPER_ONE_DIR, constant.STEPPER_ONE_STEP)
-    stepper_one.dispense(constant.STEPPER_ONE_ENABLE, constant.STEPPER_ONE_DIR, constant.STEPPER_ONE_STEP, 10)
+    stepper_one.dispense(constant.STEPPER_ONE_STEP, 10)
     stepper_two = StepperMotor()
     stepper_two.setup(constant.STEPPER_TWO_ENABLE, constant.STEPPER_TWO_DIR, constant.STEPPER_TWO_STEP)
-    stepper_two.dispense(constant.STEPPER_TWO_ENABLE, constant.STEPPER_TWO_DIR, constant.STEPPER_TWO_STEP, 10)
+    stepper_two.dispense(constant.STEPPER_TWO_STEP, 10)
     
