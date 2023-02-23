41b5f79cc06acdcad897154945cb6a7b226dd3a4
tanhuynh226 <tanhuynh226@yahoo.com>
2023-02-22 16:18:23 -0800

    remove ultrasonic deadlock in threads

--- main.py
+++ main.py
@@ -39,28 +39,24 @@ def dispenser_one(pet_one, dispenses_per_day, amount_dispensed, increments):
     global global_pet, pet_one_fed_status
     while True:
         pet_one_fed_status = False
-        # Testing "motion detection" by comparing distance before and after motion
-        dist_after_motion = ultrasonic_pet_detect.distance(constant.ULTRASONIC_TRIGGER_PET, constant.ULTRASONIC_ECHO_PET)
         #print('Measured distance = %.1f cm' % dist_after_motion)
-        print('Dispenser 1 checking if motion is detected')
-        if (dist_before_motion - dist_after_motion) > constant.MOTION_DISTANCE_THRESHOLD:
-            print('Dispenser 1 checking if motion is pet matches')
-            mask_one = np.in1d(global_pet, pet_one)
-            for value in mask_one:
-                if value == True:
-                    print('Dispenser 1 pet match confirm')
-                    i = 0
-                    while i < increments:
-                        stepper_one.dispense(constant.STEPPER_ONE_STEP, amount_dispensed/increments)
-                        time.sleep(5)
-                        i += 1
-                    pet_one_fed_status = True
-                    dispensed_notif('1', phone_number)
-                    # Testing for "pet dispenser", if it is greater than constant REFILL_DISTANCE_THRESHOLD from top of dispenser
-                    if ultrasonic_dispenser_one.distance(constant.ULTRASONIC_TRIGGER_DISPENSER_ONE, constant.ULTRASONIC_ECHO_DISPENSER_ONE) > constant.REFILL_DISTANCE_THRESHOLD:
-                        dispenser_refill_notif('1', phone_number)
-                    time.sleep(86400 / dispenses_per_day)
-                    break
+        mask_one = np.in1d(global_pet, pet_one)
+        for value in mask_one:
+            if value == True:
+                print('Dispenser 1 pet match confirm')
+                i = 0
+                while i < increments:
+                    stepper_one.dispense(constant.STEPPER_ONE_STEP, amount_dispensed/increments)
+                    time.sleep(5)
+                    i += 1
+                pet_one_fed_status = True
+                dispensed_notif('1', phone_number)
+                # Testing for "pet dispenser", if it is greater than constant REFILL_DISTANCE_THRESHOLD from top of dispenser
+                if ultrasonic_dispenser_one.distance(constant.ULTRASONIC_TRIGGER_DISPENSER_ONE, constant.ULTRASONIC_ECHO_DISPENSER_ONE) > constant.REFILL_DISTANCE_THRESHOLD:
+                    dispenser_refill_notif('1', phone_number)
+                global_pet = []
+                time.sleep(86400 / dispenses_per_day)
+                break
         
         time.sleep(.5)
 
@@ -68,45 +64,45 @@ def dispenser_two(pet_two, dispenses_per_day, amount_dispensed, increments):
     global global_pet, pet_two_fed_status
     while True:
         pet_two_fed_status = False
-        # Testing "motion detection" by comparing distance before and after motion
-        dist_after_motion = ultrasonic_pet_detect.distance(constant.ULTRASONIC_TRIGGER_PET, constant.ULTRASONIC_ECHO_PET)
         #print('Measured distance = %.1f cm' % dist_after_motion)
-        if (dist_before_motion - dist_after_motion) > constant.MOTION_DISTANCE_THRESHOLD:
-            mask_two = np.in1d(global_pet, pet_two)
-            for value in mask_two:
-                if value == True:
-                    print('Dispenser 2 pet match confirm')
-                    i = 0
-                    while i < increments:
-                        stepper_two.dispense(constant.STEPPER_TWO_STEP, amount_dispensed/increments)
-                        time.sleep(5)
-                        i += 1
-                    pet_two_fed_status = True
-                    dispensed_notif('2', phone_number)
-                    # Testing for "pet dispenser", if it is greater than constant REFILL_DISTANCE_THRESHOLD from top of dispenser
-                    if ultrasonic_dispenser_two.distance(constant.ULTRASONIC_TRIGGER_DISPENSER_TWO, constant.ULTRASONIC_ECHO_DISPENSER_TWO) > constant.REFILL_DISTANCE_THRESHOLD:
-                        dispenser_refill_notif('2', phone_number)
-                    time.sleep(86400 / dispenses_per_day)
-                    break
+        mask_two = np.in1d(global_pet, pet_two)
+        for value in mask_two:
+            if value == True:
+                print('Dispenser 2 pet match confirm')
+                i = 0
+                while i < increments:
+                    stepper_two.dispense(constant.STEPPER_TWO_STEP, amount_dispensed/increments)
+                    time.sleep(5)
+                    i += 1
+                pet_two_fed_status = True
+                dispensed_notif('2', phone_number)
+                # Testing for "pet dispenser", if it is greater than constant REFILL_DISTANCE_THRESHOLD from top of dispenser
+                if ultrasonic_dispenser_two.distance(constant.ULTRASONIC_TRIGGER_DISPENSER_TWO, constant.ULTRASONIC_ECHO_DISPENSER_TWO) > constant.REFILL_DISTANCE_THRESHOLD:
+                    dispenser_refill_notif('2', phone_number)
+                global_pet = []
+                time.sleep(86400 / dispenses_per_day)
+                break
         
         time.sleep(.5)
 
 def motion_detected(dist_before_motion):
     global global_pet, pet_one_fed_status, pet_two_fed_status
     while True:
+        print('Checking if motion is detected')
         dist_after_motion = ultrasonic_pet_detect.distance(constant.ULTRASONIC_TRIGGER_PET, constant.ULTRASONIC_ECHO_PET)
         if ((dist_before_motion - dist_after_motion) > constant.MOTION_DISTANCE_THRESHOLD) and (pet_one_fed_status == False or pet_two_fed_status == False):
             collect_frames()
             global_pet = detect_pet()
             print('Sleeping after motion detection...')
-            time.sleep(20)
+            time.sleep(5)
 
 if __name__ == '__main__':
     try: 
         # Stores owner's phone number
         while True:
             try:
-                phone_number = input('Enter your phone number with country code: ')
+#                 phone_number = input('Enter your phone number with country code: ')
+                phone_number = str(14086149226)
                 phone_number = '+' + phone_number
                 lookup = client.lookups.v1.phone_numbers(phone_number).fetch()
                 print('Phone number saved.')
